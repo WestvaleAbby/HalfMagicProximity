8c8e07a52721c58e8cef73719779cec73c9ea32d
Abby <abigail.geaslin@gmail.com>
2022-04-24 11:41:01 -0400

    Added very basic json config file and read from it

--- HalfMagicProximity/App.cs
+++ HalfMagicProximity/App.cs
@@ -4,15 +4,21 @@
     {
         // ARGTODO: Pull from config
         const string SCRYFALL_PATH = "D:\\Personal Files\\Docs\\Magic\\HalfMagic\\VS App\\oracle-cards-20220423210218.json";
-        const bool DEBUG = true;
 
         static void Main(string[] args)
         {
-            Logger.IsDebugEnabled = DEBUG;
+            ConfigManager.Init();
 
-            CardManager cardManager = new CardManager();
+            if (ConfigManager.Valid)
+            {
+                CardManager cardManager = new CardManager();
 
-            cardManager.ParseJson(SCRYFALL_PATH);
+                cardManager.ParseJson(SCRYFALL_PATH);
+            }
+            else
+            {
+                Logger.Error($"Invalid config file!");
+            }
         }
     }
 }
--- HalfMagicProximity/ConfigManager.cs
+++ HalfMagicProximity/ConfigManager.cs
@@ -1,12 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection;
+using System.Text.Json;
 
 namespace HalfMagicProximity
 {
-    internal class ConfigManager
+    static class ConfigManager
     {
+        static string ScryfallPath;
+        static string ProximityDirectory;
+
+        static public bool Valid => !string.IsNullOrEmpty(ScryfallPath) && !string.IsNullOrEmpty(ProximityDirectory);
+
+        private const string CONFIG_FILE_NAME = "hlfconfig.json";
+
+        static public void Init()
+        {
+            try
+            {
+                string[] resources = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+
+                string? configPath = resources.FirstOrDefault(x => x.Contains(CONFIG_FILE_NAME)).Replace("HalfMagicProximity.", "");
+
+                if (string.IsNullOrEmpty(configPath)) throw new Exception($"Config File not found!");
+
+                using (Stream configStream = new FileStream(configPath, FileMode.Open, FileAccess.Read))
+                using (JsonDocument configDoc = JsonDocument.Parse(configStream))
+                {
+                    JsonElement configOptions = configDoc.RootElement.GetProperty("hlfOptions");
+
+                    Logger.IsDebugEnabled = configOptions.GetProperty("IsDebugEnabled").GetBoolean();
+
+                    ScryfallPath = configOptions.GetProperty("ScryfallPath").GetString();
+                    if (string.IsNullOrEmpty(ScryfallPath)) throw new Exception($"Path to Scryfall JSON not found!");
+
+                    ProximityDirectory = configOptions.GetProperty("ProximityDirectory").GetString();
+                    if (string.IsNullOrEmpty(ProximityDirectory)) throw new Exception($"Path to Proximity Directory not found!");
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.Error("Config Error: " + e.Message);
+            }
+        }
     }
 }
