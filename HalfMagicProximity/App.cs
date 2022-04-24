71f8339b4a92d5b795670c42d6b95174fcdd49f6
Patrick Geaslin <pjgeaslin@gmail.com>
2022-04-23 20:19:00 -0400

    Add project files.

---
+++ HalfMagicProximity/App.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class App
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
