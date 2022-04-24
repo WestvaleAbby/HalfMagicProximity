6218da32cf2b1b4c977b7c14329ef0e9a04d378c
Abby <abigail.geaslin@gmail.com>
2022-04-23 23:43:26 -0400

    Filtering out all illegal cards

--- HalfMagicProximity/CardManager.cs
+++ HalfMagicProximity/CardManager.cs
@@ -4,24 +4,100 @@ namespace HalfMagicProximity
 {
     public class CardManager
     {
+        // ARGTODO: Remove once actual card storage is implemented
+        private int cardCount = 0;
+
         public void ParseJson(string jsonPath)
         {
             // ARGTODO: Dummy proofing?
             using (Stream jsonStream = new FileStream(jsonPath, FileMode.Open, FileAccess.Read))
             using (JsonDocument jsonDoc = JsonDocument.Parse(jsonStream))
             {
-                // An array with all of the parsed card data
-                JsonElement scryfallData = jsonDoc.RootElement;
+                JsonElement root = jsonDoc.RootElement;
 
-                Logger.Info($"JSON Parsed. Found {scryfallData.GetArrayLength()} cards.");
+                if (root.GetArrayLength() == 0)
+                {
+                    Logger.Error($"No cards found in the Scryfall JSON: {jsonPath}");
+                    return;
+                }
 
-                CardData testCard = new CardData(
-                    scryfallData[0].GetProperty("name").ToString(),
-                    scryfallData[0].GetProperty("mana_cost").ToString(),
-                    CardFace.Front,
-                    CardLayout.Split);
+                Logger.Debug($"Filtering {root.GetArrayLength()} cards.");
+                for (int i = 0; i < root.GetArrayLength(); i++)
+                {
+                    JsonElement node = root[i];
 
-                Logger.Debug(testCard.GetDisplayString());
+                    // Filter cards not in the Adventure or Split layout
+                    string layout = GetProperty(node, CardProperty.Layout);
+                    if (layout != "adventure" && layout != "split") continue;
+
+                    // Filter out non black bordered cards
+                    if (GetProperty(node, CardProperty.BorderColor) != "black") continue;
+
+                    // Filter out cards from banned sets
+                    string[] bannedSetCodes =
+                    {
+                        "cmb", // Playtest Cards
+                        "htr", // Heroes of the Realm
+                    };
+                    bool illegalSetCode = false;
+                    foreach (string bannedCode in bannedSetCodes)
+                    {
+                        if (GetProperty(node, CardProperty.SetCode).Contains(bannedCode))
+                        {
+                            illegalSetCode = true; 
+                            break;
+                        }
+                    }
+                    if (illegalSetCode) continue;
+
+                    AddCard(node);
+                }
+            }
+            
+            if (cardCount == 0)
+                Logger.Error("No legal cards found!");
+            else
+                Logger.Info($"There are {cardCount} legal cards.");
+        }
+
+        private void AddCard(JsonElement jsonCard)
+        {
+            // ARGTODO: Pull relevant card data from JSON
+            cardCount++;
+            Logger.Debug($"{GetProperty(jsonCard, CardProperty.Name)} is legal.");
+        }
+
+        // Extract the value of a json element's property as a string
+        private string GetProperty(JsonElement element, CardProperty property)
+        {
+            return element.GetProperty(PropertyString(property)).ToString();
+        }
+
+        // Enum to facilitate accessing specific json card properties. Maintain alphabetization
+        private enum CardProperty
+        {
+            Artist,
+            BorderColor,
+            Layout,
+            ManaCost,
+            Name,
+            SetCode,
+        };
+
+        // Converts CardProperty enum to the string found in the scryfall json
+        private string PropertyString(CardProperty property)
+        {
+            switch (property)
+            {
+                case CardProperty.Name: return "name";
+                case CardProperty.ManaCost: return "mana_cost";
+                case CardProperty.Layout: return "layout";
+                case CardProperty.Artist: return "artist";
+                case CardProperty.BorderColor: return "border_color";
+                case CardProperty.SetCode: return "set";
+                default:
+                    Logger.Error($"Tried to access a card property that doesn't exist: {property}");
+                    return "";
             }
         }
     }
